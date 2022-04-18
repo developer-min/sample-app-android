38b0bf5abeba83f731ed6714a2b1957ea2a0ddd2
min_developer <ichigo.sk6@gmail.com>
2022-04-19 03:37:58 +0900

    Added TopFragment.

--- app/src/main/java/com/example/sampleapp/MainActivity.kt
+++ app/src/main/java/com/example/sampleapp/MainActivity.kt
@@ -2,11 +2,19 @@ package com.example.sampleapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fragment = TopFragment()
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.add(R.id.container, fragment)
+        transaction.commit()
     }
 }
